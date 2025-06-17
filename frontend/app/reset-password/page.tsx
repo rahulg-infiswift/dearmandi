243b9364e1944720e8ed0717f63d1b843a5c6215
rahulg-infiswift <rahul@infiswift.tech>
2025-06-17 01:35:50 -0700

    Fix suspense boundry error for reset-password page

--- frontend/app/reset-password/page.tsx
+++ frontend/app/reset-password/page.tsx
@@ -1,10 +1,19 @@
 import { ResetPasswordPage } from "@/components/ResetPasswordPage";
-import React from "react";
+import React, { Suspense } from "react";
+
+// Loading component to show while the ResetPasswordPage is loading
+const ResetPasswordLoading = () => (
+  <div className="h-screen flex justify-center items-center">
+    <p>Loading reset password form...</p>
+  </div>
+);
 
 const page = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <ResetPasswordPage />
+      <Suspense fallback={<ResetPasswordLoading />}>
+        <ResetPasswordPage />
+      </Suspense>
     </div>
   );
 };
